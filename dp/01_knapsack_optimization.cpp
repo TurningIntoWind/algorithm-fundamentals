23653ac123c4f7c751939228468c11b7c3aa2262
first rays of the morning sun <164125845+TurningIntoWind@users.noreply.github.com>
2025-10-06 16:12:59 +0800

    Add files via upload

---
+++ dp/01_knapsack_optimization.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 1010;
+int v[N], w[N];
+int dp[N];
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> v[i] >> w[i];
+
+    // dp[j]表示前i件物品装进容量为j的背包的最大价值
+    // dp[j] = max{dp[j], dp[j-v[i]] + w[i]}
+    for (int i = 1; i <= n; i++)
+        for (int j = m; j >= v[i]; j--)
+            dp[j] = max(dp[j], dp[j - v[i]] + w[i]);
+
+    cout << dp[m] << endl;
+
+    return 0;
+}
